3f8a743df412b6647c7e90c9276558f0eaf37214
Anirudh Kalia <anirudhkalia@Anirudhs-MacBook-Pro.local>
2023-01-05 00:02:34 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is my personal portfolio/website. It was built using Svelte for the front-end, Hygraph for content management. It's currently hosted using Vercel.
+
+
---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,45 @@
+<script context="module">
+  import {
+    authorsStore,
+    fetchAuthors,
+    fetchSocials,
+    socialsStore,
+  } from '$stores/site-metadata'
+  import LinkedinIcon from './linkedin-icon.svelte'
+  import GithubIcon from './github-icon.svelte'
+  import YouTubeIcon from './you-tube-icon.svelte'
+
+  fetchAuthors()
+  fetchSocials()
+</script>
+
+<script>
+  const { githubUrl, linkedinUrl } = $socialsStore
+  const { name: AuthorName } = $authorsStore
+</script>
+
+<footer
+  class="p-10 footer bg-primary text-primary-content footer-center"
+>
+  <div class="grid grid-flow-col gap-4">
+    <a class="link link-hover" href="/projects">Portfolio</a>
+    <a class="link link-hover" href="/posts">Blog</a>
+    <a class="link link-hover" href="/about">About</a>
+  </div>
+  <div>
+    <div class="grid grid-flow-col gap-4">
+      <a href={githubUrl}>
+        <GithubIcon />
+      </a>
+      <a href={linkedinUrl}>
+        <LinkedinIcon />
+      </a>
+    </div>
+  </div>
+  <div>
+    <p>
+      Copyright &copy; {`${new Date().getFullYear()}`} - All right reserved
+      {AuthorName}
+    </p>
+  </div>
+</footer>
